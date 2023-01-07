c40e39c48c0d7228b7878eeccacfa33591df9f70
Kuwar Pratap Singh <guru75000@gmail.com>
2023-01-07 14:54:01 +0530

    update

--- client/script.js
+++ client/script.js
@@ -89,7 +89,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:3010/', {
+    const response = await fetch('https://kubekode-aervice.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
