4f1ad5fa40124a4a7cdc36615c66a565a7a2ace5
Kuwar Pratap Singh <guru75000@gmail.com>
2023-01-07 16:20:35 +0530

    update

--- client/script.js
+++ client/script.js
@@ -2,19 +2,19 @@ import kuwarp from './assets/KUWARP.gif'
 import user from './assets/bgr.jpg'
 
 const form = document.querySelector('#kuwarp-form')
-const chatContainer = document.querySelector('#load_container')
+const chatContainer = document.querySelector('#message_container')
 
-let loadInterval; 
+let messageInter; 
 
 function loader(element) {
     element.textContent = ''
 
-    loadInterval = setInterval(() => {
+    messageInter = setInterval(() => {
       
-        element.textContent += '📺';
+        element.textContent += '🤔';
 
-        // If the loading indicator has reached three dots, reset it
-        if (element.textContent === '📺📺📺📺📺') {
+      
+        if (element.textContent === '🤔🤔🤔🤔') {
             element.textContent = '';
         }
     }, 300);
@@ -22,9 +22,7 @@ function loader(element) {
 
 function typeText(element, text) {
     let index = 0
-    // if(index>1){
-    //    return (prompt('enter first')) 
-    // }
+   
 
     let interval = setInterval(() => {
         if (index < text.length) {
@@ -36,10 +34,8 @@ function typeText(element, text) {
     }, 20)
 }
 
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
-function generateUniqueId() {
+
+function generateuniReq() {
     const timestamp = Date.now();
     const randomNumber = Math.random();
     const hexadecimalString = randomNumber.toString(16);
@@ -47,18 +43,19 @@ function generateUniqueId() {
     return `id-${timestamp}-${hexadecimalString}`;
 }
 
-function chatStripe(isAi, value, uniqueId) {
+function chatStripe(isAi, value, uniReq) {
     return (
         `
         <div class="wrapper ${isAi && 'ai'}">
             <div class="chat">
                 <div class="profile">
                     <img 
+                    class="chatbot"
                       src=${isAi ?  kuwarp : user} 
                       alt="${isAi ? 'kuwarp' : 'user'}" 
                     />
                 </div>
-                <div class="message" id=${uniqueId}>${value}</div>
+                <div class="message" id=${uniReq}>${value}</div>
             </div>
         </div>
     `
@@ -70,24 +67,23 @@ const handleSubmit = async (e) => {
 
     const data = new FormData(form)
 
-    // user's chatstripe
+
     chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
 
-    // to clear the textarea input 
+
     form.reset()
 
-    // bot's chatstripe
-    const uniqueId = generateUniqueId()
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+   
+    const uniReq = generateuniReq()
+    chatContainer.innerHTML += chatStripe(true, " ", uniReq)
 
-    // to focus scroll to the bottom 
     chatContainer.scrollTop = chatContainer.scrollHeight;
 
-    // specific message div 
-    const messageDiv = document.getElementById(uniqueId)
+  
+    const messageWindow = document.getElementById(uniReq)
 
-    // messageDiv.innerHTML = "..."
-    loader(messageDiv)
+   
+    loader(messageWindow)
 
     const response = await fetch('https://kubekode-aervice.onrender.com/', {
         method: 'POST',
@@ -99,18 +95,18 @@ const handleSubmit = async (e) => {
         })
     })
 
-    clearInterval(loadInterval)
-    messageDiv.innerHTML = " "
+    clearInterval(messageInter)
+    messageWindow.innerHTML = " "
 
     if (response.ok) {
         const data = await response.json();
-        const parsedData = data.kuwarp.trim() // trims any trailing spaces/'\n' 
+        const parsedData = data.kuwarp.trim()  
 
-        typeText(messageDiv, parsedData)
+        typeText(messageWindow, parsedData)
     } else {
         const err = await response.text()
 
-        messageDiv.innerHTML = "I think you need to try something new";
+        messageWindow.innerHTML = "I think you need to try something new";
         alert(err)
     }
 }
