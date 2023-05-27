f885ad844285fa2ab59edcdd86c321688d100198
Kuwar Pratap Singh <127750700+Creditklick@users.noreply.github.com>
2023-05-27 17:46:51 +0530

    Feat(fe):Key reload

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
    
     loader(messageWindow)
 
-    const response = await fetch('https://kubekode-aervice.onrender.com/', {
+    const response = await fetch('http://localhost:3010', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
